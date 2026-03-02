637f158e8b23946323226b4b1c0cc869713347ad
tanishka-taras <tanishkataras30@gmail.com>
2026-03-02 14:12:21 +0000

    2/3/26

---
+++ operators.py
@@ -0,0 +1,49 @@
+a=30
+b=20
+
+print("calculate the following")
+
+print("ARTHIMATIC OPERATORS")
+print('Addition =',a+b)
+print('subtraction =',a-b)
+print('Multiplication =',a*b)
+print('Division =',a%b)
+print('Modules =',a/b)
+print('Floor division =',a//b)
+print('Exponent =',a**b)
+
+print("COMPARISON OPERATORS")
+print('Equal to =',a==b)
+print('Not equal to =',a!=b)
+print('Greater than equal to =',a>=b)
+print('Less than equal to =',a<=b)
+print('Greater than =',a>b)
+print('Less than =',a<b)
+
+
+
+print("ASSIGNMENT OPERATORS")
+print('a+=b :',a+b)
+print('a-=b: =',a-b)
+print('a*=b =',a*b)
+print('a/=b=',a+b)
+print('a%=b:',a%b)
+print('a**=b',a**b)
+print('a//=b',a//b)
+
+print("LOGICAL OPERATORS")
+print('\"a > 5 and a < 7\" =>', a > 5 and a < 7)      
+print('\"a > 5 or a < 7\" =>', a > 5 or a < 7)    
+print('\"not (a > 5 and a < 7)\" =>', not(a > 5 and a < 7))      
+
+
+
+print('BITWISE OPERATOR')
+print('bitwise AND=',a&b)
+print('bitwise  OR=',a|b)
+print('bitwise XOR=',a^b)
+print('bitwise NOT=',~a)
+print('bitwise Right shift=',a<<b)
+print('bitwise Left shift=',a>>b)
+
+
