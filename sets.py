8f9b50ad68705e867ec9f20f9189e21cf50197bb
tanishka-taras <tanishkataras30@gmail.com>
2026-03-30 13:59:37 +0000

    30/3/26

---
+++ sets.py
@@ -0,0 +1,22 @@
+set1 ={1,2,3,4,5,6,7,8}
+
+print(set1)
+print(type(set1))
+
+set1.add("Maths")
+print(set1)
+
+set1.update(["Science","hindi","english"])
+print(set1)
+
+
+set1.remove('hindi')
+
+set1.discard(8)
+print(set1)
+
+set1.pop()
+print(set1)
+
+set1.clear()
+print(set1)
